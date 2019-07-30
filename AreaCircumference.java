b1bfe26ece0e4c85ba95982297c9a8db31ada928
priyankaubale <52989407+priyankaubale@users.noreply.github.com>
2019-07-30 23:20:12 +0530

    Add files via upload

---
+++ AreaCircumference.java
@@ -0,0 +1,11 @@
+class AreaCircumference
+{ 
+ public static void main(String args[ ])
+{
+ double radius=10.23;
+ double area=3.14*radius*radius;
+ double circum=2*3.14*radius;
+ System.out.print(area);
+ System.out.print(circum);
+}
+}
